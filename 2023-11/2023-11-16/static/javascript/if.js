c78e311a6eb81dcbd61c0fd1abedde0904a8787d
kyerim1 <nilzero25@gmail.com>
2023-11-16 12:47:16 +0900

    가위바위보

--- 2023-11/2023-11-16/static/javascript/if.js
+++ 2023-11/2023-11-16/static/javascript/if.js
@@ -75,3 +75,81 @@ if( 조건식 ){
 
 
 // 동전 앞면 뒷면 맞추기
+
+//  css 선택자  id- #,  class - .
+
+// 브라우저에 html태그가 모두 로딩(화면표시)되면 자바스크립트 코드를 실행시키는방법
+
+window.onload=function(){
+
+    // 가위 바위 보 게임 만들기 
+    // 1.가위  2.바위  3.보
+
+    var com = parseInt(Math.random()*3)+1;
+    var user = parseInt(prompt("1.가위  2.바위  3.보"));
+
+    if( com == user){
+        alert("비김");
+    }else if( (com==1&&user==2) || (com==2 && user==3) || (com==3 && user==1)  ){
+        alert("승");
+    }else{
+        alert("패");
+    }
+
+
+
+    //세개의 정수를 입력받아 가장 작은수 출력 , 모두 같다면 같다 출력
+    // var num1= parseInt(prompt("첫번째 정수"));
+    // var num2 = parseInt(prompt("두번째 정수 "));
+    // var num3 = parseInt(prompt("세번째 정수 "));
+
+    // if( num1 == num2 && num1 == num3){
+    //     alert("모두 같은숫자");
+    // }else if( (num1<num2 || num1==num2 ) && (num1<num3 || num1==num3) ){
+    //     alert("작은수 : "+num1);
+    // }else if( (num2<num1 || num2==num1) && (num2<num3|| num2==num3) ){
+    //     alert("작은수 : " + num2);
+    // }else if( num3<num1 && num3<num2 ){
+    //     alert("작은수 : "+ num3);
+    // }
+
+    // if(num1 < num2){
+    //     if( num1 < num3 ){
+    //          alert("작은수 : " +  num1);
+    //     }else{
+    //         alert("작은수 : "+num3);
+    //     }
+    // }else if( num2 < num3){
+    //     alert("작은수 : " +num2);
+    // }else{
+    //     alert("작은수 : "+num3);
+    // }
+
+
+
+
+    // var coin = Math.floor(Math.random()*2)+1;
+
+    // //javascript html태그 선택방법
+    // var front = document.getElementById("front");
+    // var back = document.getElementById("back");
+
+    // front.addEventListener("click", function(){
+    //     if( coin == 1 ){
+    //         alert(" 정답 !");
+    //     }else{
+    //         alert("땡!!");
+    //     }
+    //     //alert(" 앞면 선택 ");  // alert는 알림창을 나타내는 함수이다.
+    // });
+
+    // back.addEventListener("click",function(){
+    //     if( coin == 2 ){
+    //         alert("정답!");
+    //     }else{
+    //         alert("땡!");
+    //     }
+    //     //alert("뒷면 선택");
+    // });
+};
+
